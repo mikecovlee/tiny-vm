909ed0a0ef72a123cefc695be9236ac5795ff908
Michael Lee <mikecovlee@163.com>
2018-10-13 09:40:00 +0800

    暂时完成一部分

--- bytecode.md
+++ bytecode.md
@@ -4,4 +4,17 @@
 | Mnemonic | Opcode (in hex) | Other bytes ([count]: [operand labels]) | Description |
 |:--------:|:---------------:|:---------------------------------------:|:-----------:|
 |nop|00|none|Do noting|
-|push_i8|01|
+|pop|01|none|Pop top element from stack|
+|push_i32|02|4:bytes|Push an 32bit integer into stack|
+|push_i64|03|8:bytes|Push an 64bit integer into stack|
+|add_i32|04|none|Add two element|
+|add_i64|05|none|Add two element|
+|sub_i32|06|none|Subtract two element|
+|sub_i64|07|none|Subtract two element|
+|int|08|8:index|Call interrupt|
+
+### Interrupts
+| Mnemonic | Opcode (in hex) | Other bytes ([count]: [operand labels]) | Description |
+|:--------:|:---------------:|:---------------------------------------:|:-----------:|
+|Print_i32|01|4:bytes|Print top element|
+|Print_i64|02|8:bytes|Print top element|
--- vm.hpp
+++ vm.hpp
@@ -1,6 +1,59 @@
 #pragma once
+#include <cstdlib>
+#include <cstdint>
 
 namespace vm
 {
-    
+    namespace types
+    {
+        using i32=std::int32_t;
+        using i64=std::int64_t;
+    }
+    using byte_t=unsigned char;
+    using size_t=unsigned long;
+    class allocator_base
+    {
+    public:
+        allocator_base()=default;
+        virtual ~allocator_base()=default;
+        virtual byte_t* malloc(size_t)=0;
+        virtual void free(byte_t*)=0;
+    };
+    class default_allocator final:public allocator_base
+    {
+    public:
+        virtual byte_t* malloc(size_t size) override
+        {
+            return reinterpret_cast<byte_t*>(::malloc(size));
+        }
+        virtual void free(byte_t* ptr) override
+        {
+            ::free(reinterpret_cast<void*>(ptr));
+        }
+    };
+    class instance_base;
+    class instruction_base;
+    class instance_base
+    {
+        allocator_base* m_alloc=new default_allocator;
+    public:
+        instance_base()=default;
+        explicit instance_base(allocator_base* alloc):m_alloc(alloc) {}
+        virtual ~instance_base(){delete m_alloc;}
+    };
+    class instruction_base
+    {
+    public:
+        instruction_base()=default;
+        virtual ~instruction_base()=default;
+        virtual size_t get_args_byte_count() const
+        {
+            return 0;
+        }
+        virtual void execute(byte_t*, instance_base*)=0;
+    };
+    class bytecode_interpreter final
+    {
+
+    };
 }
